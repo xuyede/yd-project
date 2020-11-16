c945c110fb08c0b16a1cbe13e3b769c8f0d6a426
deye <will3virgo@163.com>
2020-11-17 00:11:58 +0800

    fix:去除error_handler console信息

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,11 +1,9 @@
 function handler(app) {
   app.use(async function(ctx, next) {
     try {
-      console.log(111)
       await next();
     } catch (err) {
       //TODO 把错误记录到日志
-      console.log('🤓️', err)
       const status = err.status || 500;
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
       const error = status === 500 && process.env.NODE_ENV === 'production'
