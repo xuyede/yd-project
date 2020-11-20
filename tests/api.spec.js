9c81094cc94411b777f769dbf2f151d509fe32f4
xuyede <yede.xu@jyblife.com>
2020-11-20 17:18:03 +0800

    fix:test

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,16 +1,14 @@
 const request = require('supertest');
-const Koa = require('koa');
-const expect = require('chai').expect;
-const app = new Koa();
 
 describe('接口测试', function() {
   it('/api/post', function(done) {
-    request('http://169.254.100.0:12306')
+    request('http://172.16.5.121:12306')
       .post('/api/post')
       .send({name: 'yd'})
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, res) {
+        console.log(res.body.data)
         if (err) return done(err);
         done();
       })
